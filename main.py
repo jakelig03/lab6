ed1489a26810ba76909dbc974d62d9fc2f8f15fa
jakelig03 <jake.liguori@ufl.edu>
2023-10-30 13:48:06 -0400

    Delete .idea directory

--- main.py
+++ main.py
@@ -20,12 +20,6 @@ if __name__ == '__main__':
             norm_pass = input("Please enter your password to encode:")
             print("Your password has been encoded and stored!")
         if option == "2":
-            code_pass = None
-            for char in norm_pass:
-                char = int(char) + 3
-                if char >= 10:
-                    char -= 10
-                code_pass = str(code_pass) + str(char)
-            print(f"The encoded password is {code_pass}, and the original password is {norm_pass}.")
+
         if option == "3":
             break
